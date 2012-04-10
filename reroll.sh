fefd8c4d31de3375024f0fa5ba5ac7576beec432
Jesper Mathiassen <jm@bellcom.dk>
2012-04-10 12:03:53 +0200

    Remove dev checkouts

--- reroll.sh
+++ reroll.sh
@@ -3,4 +3,4 @@
 export http_proxy=http://localhost:3128/
 export ftp_proxy=http://localhost:3128/
 
-./os2web_build.py -D  -m profile -l os2web-`date +%Y%m%d%H%M`
+./os2web_build.py -m profile -l os2web-`date +%Y%m%d%H%M`
