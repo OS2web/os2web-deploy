f531529e8f44df760e7a1db40864e7a5297a772f
Jesper Mathiassen <jm@bellcom.dk>
2013-03-06 16:39:30 +0100

    Cleanup

--- reroll-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#export http_proxy=http://localhost:3128/
-#export ftp_proxy=http://localhost:3128/
-
-./os2web_build.py -D -m profile -f os2web.dev.make -L dev -l os2web-dev-`date +%Y%m%d%H%M`
--- reroll-test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#export http_proxy=http://localhost:3128/
-#export ftp_proxy=http://localhost:3128/
-
-./os2web_build.py -m profile -f os2web.dev.make -t os2web-test-`date +%Y%m%d%H%M`
