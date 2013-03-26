bc48017a7b0d71f628747babe5208a66d9f108a2
Anders Bryrup <andersbryrup@gmail.com>
2013-03-26 13:12:37 +0100

    Include a different .info file dependent to which build

--- reroll.turnkey.sh
+++ reroll.turnkey.sh
@@ -7,7 +7,7 @@ drush make --no-gitinfofile -y --no-core --contrib-destination=os2web os2web.tur
 
 cd os2web
 
-ln -s ../os2web.info os2web.info
+ln -s ../os2web.turnkey.info os2web.info
 ln -s ../os2web.turnkey.profile os2web.profile
 ln -s ../os2web.turnkey.install os2web.install
 ln -s ../db.sql.gz db.sql.gz
